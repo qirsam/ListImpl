9cff2cdfd91ecc07d743f285e8ab8923289cbbb6
qirsam <qirsam@gmail.com>
2022-12-14 21:07:42 +0300

    test

--- src/main/java/com/qirsam/collectionsImpl/Main.java
+++ src/main/java/com/qirsam/collectionsImpl/Main.java
@@ -4,30 +4,6 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-
-        int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
-
-        ListImpl<Integer> list = new ListImpl<>();
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
-        }
-
-        extracted(list);
-
-        list.sort(Comparator.reverseOrder());
-
-        extracted(list);
-
-        list.add(10, 13);
-        extracted(list);
-
-
-    }
-
-    private static void extracted(ListImpl<Integer> list) {
-        for (int i = 0; i < list.size(); i++) {
-            System.out.printf("%d ", list.get(i));
-        }
-        System.out.println();
+        System.out.println("Start Application");
     }
 }
---
+++ src/test/java/com/qirsam/collectionsImpl/ListImplTest.java
@@ -0,0 +1,39 @@
+package com.qirsam.collectionsImpl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ListImplTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+}
